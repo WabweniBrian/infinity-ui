97f1cc9e6ec1f60fd0dc3b1fbda37ce48fd5a87b
WabweniBrian <wabzbraize66@gmail.com>
2025-03-24 15:13:05 +0300

    Updates

--- app/(main)/(preview)/preview/[category]/[slug]/page.tsx
+++ app/(main)/(preview)/preview/[category]/[slug]/page.tsx
@@ -1,7 +1,13 @@
 import { prisma } from "@/lib/prisma";
-import { formatWord } from "@/lib/utils";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+
+const getComponent = async (slug: string) => {
+  return await prisma.component.findUnique({
+    where: { slug, show: true },
+    select: { Componentpath: true, name: true },
+  });
+};
 
 export async function generateMetadata({
   params,
@@ -10,8 +16,10 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const slug = params.slug;
   try {
+    const component = await getComponent(slug);
+    if (!component) notFound();
     return {
-      title: formatWord(slug),
+      title: component.name,
     };
   } catch (error) {
     return notFound();
@@ -36,10 +44,7 @@ const loadComponent = async (componentPath: string) => {
 
 export default async function PreviewPage({ params }: PreviewPageProps) {
   const { slug } = params;
-  const componentData = await prisma.component.findUnique({
-    where: { slug, show: true },
-    select: { Componentpath: true },
-  });
+  const componentData = await getComponent(slug);
 
   if (!componentData) {
     notFound();
